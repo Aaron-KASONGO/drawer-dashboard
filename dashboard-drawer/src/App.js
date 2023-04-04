636c1218cc14b3425510680cec6b163519494ae9
Aaron-KASONGO <66473473+Aaron-KASONGO@users.noreply.github.com>
2023-04-04 19:55:38 +0000

    Client page component

--- dashboard-drawer/src/App.js
+++ dashboard-drawer/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ToolBar } from './components/ToolBar';
@@ -10,9 +10,9 @@ import { HomePage } from './components/Content/HomePage';
 import { ReservationPage } from './components/Content/ReservationPage';
 import { SallesPages } from './components/Content/SallesPages';
 import { HistoriquePage } from './components/Content/HistoriquePage';
+import { ClientPage } from './components/Content/ClientPage';
 
 
-const drawerWidth = 240;
 
 const App = () => {
   const theme = useTheme();
@@ -38,7 +38,7 @@ const App = () => {
             <Route path='reservations' element={<ReservationPage open={open} />} />
             <Route path='salles' element={<SallesPages open={open} />} />
             <Route path='historique' element={<HistoriquePage open={open} />} />
-            <Route path='clients' element={<SallesPages open={open} />} />
+            <Route path='clients' element={<ClientPage open={open} />} />
           </Route>
         </Routes>
       </Box>
---
+++ dashboard-drawer/src/components/Content/ClientPage.js
@@ -0,0 +1,84 @@
+import { Typography, styled, Grid, Card, CardMedia, CardContent, CardActions, IconButton, Avatar, ButtonGroup, Button, Tooltip } from "@mui/material";
+
+import LockPersonIcon from '@mui/icons-material/LockPerson';
+import InfoIcon from '@mui/icons-material/Info';
+
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
+
+export const ClientPage = ({open}) => {
+  return (
+    <Main open={open}>
+        <DrawerHeader />
+        <Grid container rowSpacing={2} columnSpacing={1}>
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Card>
+              <CardContent
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                mt: 2
+              }}
+              >
+                <Avatar src="" sx={{
+                    width: 60,
+                    height: 60,
+                    my: 1
+                }} alt="Avatar" />
+                <Typography variant="body1">KALUNDA Jules</Typography>
+              </CardContent>
+              <CardActions>
+                <ButtonGroup
+                orientation="horizontal"
+                variant="outlined"
+                size="small"
+                fullWidth
+                >
+                    <Tooltip title="Détail du contact">
+                        <Button>
+                            <InfoIcon />
+                        </Button>
+                    </Tooltip>
+                    <Tooltip title="Blocker le contact">
+                        <Button color="error">
+                            <LockPersonIcon />
+                        </Button>
+                    </Tooltip>
+                </ButtonGroup>
+              </CardActions>
+            </Card>
+          </Grid>
+
+        </Grid>
+    </Main>
+  )
+}
